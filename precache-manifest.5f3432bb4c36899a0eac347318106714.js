2bd207802c4d29e2b701cd413531c5caf52ce94e
amie-affection <amie.affection@gmail.com>
2020-06-28 20:45:09 +0300

    Updates

---
+++ precache-manifest.5f3432bb4c36899a0eac347318106714.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432531a4c5dd3d1f9424322e5418f980",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "f19809d034254596eb1b",
+    "url": "/Weather/static/js/2.8ef84931.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.8ef84931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace2f7bfae3cb8e05874",
+    "url": "/Weather/static/js/main.0bd5051c.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  }
+]);
